63f4ade6e0f982c9f877e8a5393858719840cbc9
admin <admin@DESKTOP-OGI0BRA>
2017-07-04 19:32:49 +0530

    new changes by naren

--- Framework/src/test/Browser.java
+++ Framework/src/test/Browser.java
@@ -4,6 +4,12 @@ public class Browser {
 
 	public static void main(String[] args) {
 		System.out.println("my test project ");
+		System.out.println("my test project1 ");
+		System.out.println("my test project ");
+		System.out.println("my test project ");
+		System.out.println("my test project ");
+		System.out.println("my test project ");
+		System.out.println("my test project ");
 
 	}
 
