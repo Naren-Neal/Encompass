c637f7f6c15e1e5fa11dc85b7ffcb4c5a5686ef0
admin <admin@DESKTOP-OGI0BRA>
2017-07-05 11:20:21 +0530

    ssasas

--- Framework/src/test/Browser.java
+++ Framework/src/test/Browser.java
@@ -5,10 +5,7 @@ public class Browser {
 	public static void main(String[] args) {
 		System.out.println("my test project ");
 		System.out.println("my test project1 ");
-		System.out.println("my test project ");
-		System.out.println("my test project ");
-		System.out.println("my test project ");
-		System.out.println("my test project ");
+		
 		
 
 	}
