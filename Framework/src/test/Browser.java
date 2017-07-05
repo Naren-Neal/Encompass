13afbd236a66c0a74142c13823cd223cb60bf4fa
admin <admin@DESKTOP-OGI0BRA>
2017-07-05 14:42:04 +0530

    revert

--- Framework/src/test/Browser.java
+++ Framework/src/test/Browser.java
@@ -5,7 +5,7 @@ public class Browser {
 	public static void main(String[] args) {
 		System.out.println("my test project ");
 		System.out.println("my test project1 ");
-		System.out.println("my test project11233 ");
+		
 		
 		
 
